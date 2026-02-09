9dfdeafd740d8217d06404b6881e6958f2ef17f7
Artemy Egorov <artegoser@gmail.com>
2026-02-09 11:45:55 +0300

    browser version

---
+++ src/browser.ts
@@ -0,0 +1,24 @@
+export { Schedule } from "./tt/schedule.js";
+export {
+  getSemesterStart,
+  getSemesterWeeks,
+  getWeekNumber,
+  getWeekdayName,
+} from "./tt/utils.js";
+export { Period, EducationType } from "./common/types.js";
+export type {
+  Time,
+  WeekRange,
+  Teacher,
+} from "./common/types.js";
+export type {
+  Faculty,
+  Group,
+  ScheduleEntry,
+  FullScheduleSlot,
+  FullScheduleDay,
+  LessonTimeSlot,
+  Lesson,
+  LessonTime,
+  SemesterWeek,
+} from "./tt/types.js";
